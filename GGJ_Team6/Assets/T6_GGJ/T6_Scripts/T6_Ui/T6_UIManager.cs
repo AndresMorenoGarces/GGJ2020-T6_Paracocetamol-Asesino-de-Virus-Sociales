7d73020361dcbb93e2e1e9969e27d92e39ee49a2
Luxfero000 <andres999moreno@outlook.com>
2020-02-01 11:57:54 -0500

    Text MEsh Pro Incorporated

--- GGJ_Team6/Assets/T6_GGJ/T6_Scripts/T6_Ui/T6_UIManager.cs
+++ GGJ_Team6/Assets/T6_GGJ/T6_Scripts/T6_Ui/T6_UIManager.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class T6_UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartButton()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("T6_Main");
     }
 }
