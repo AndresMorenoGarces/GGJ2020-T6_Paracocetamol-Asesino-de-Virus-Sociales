30b8c165e96d52e33003d3f04092ff8414467601
Luxfero000 <andres999moreno@outlook.com>
2020-02-02 15:35:11 -0500

    Ultima

---
+++ GGJ_Team6/Assets/T6_GGJ/T6_Scripts/T6_Ui/T6_UICredits.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class T6_UICredits : MonoBehaviour
+{
+    public void MainMenuButton()
+    {
+        Time.timeScale = 1;
+        LoadScenes(0);
+    }
+    private void LoadScenes(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
---
+++ GGJ_Team6/Assets/T6_GGJ/T6_Scripts/T6_Ui/T6_UIMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class T6_UIMenu : MonoBehaviour
+{
+    public void StartButton()
+    {
+        LoadScenes(1);
+    }
+
+    public void CreditsButton()
+    {
+        LoadScenes(2);
+    }
+
+    private void LoadScenes(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
