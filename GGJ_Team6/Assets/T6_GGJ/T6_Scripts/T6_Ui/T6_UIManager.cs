a1cb261ba69bd771da9d71d098b39c833f1b56ce
Luxfero000 <andres999moreno@outlook.com>
2020-02-01 20:39:42 -0500

    Animations Update/ UI Interface

--- GGJ_Team6/Assets/T6_GGJ/T6_Scripts/T6_Ui/T6_UIManager.cs
+++ GGJ_Team6/Assets/T6_GGJ/T6_Scripts/T6_Ui/T6_UIManager.cs
@@ -7,8 +7,38 @@ using UnityEngine.SceneManagement;
 
 public class T6_UIManager : MonoBehaviour
 {
+    private bool active = false;
+
+    public void MainMenuButton()
+    {
+        Time.timeScale = 1;
+        LoadScenes(0);
+    }
     public void StartButton()
     {
-        SceneManager.LoadScene("T6_Main");
+        LoadScenes(1);
     }
+
+    public void CreditsButton()
+    {
+        LoadScenes(2);
+    }
+    public void SettingsButton()
+    {
+        active =! active;
+        Time.timeScale = (active) ? 0 : 1;
+    }
+
+    private void LoadScenes(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    public void RestartGameButton()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    
 }
