e5268921e23a1348930ba52badb50aedb228f8d2
AmayaJuan <jpva360@gmail.com>
2020-02-02 11:46:37 -0500

    Audio

---
+++ GGJ_Team6/Assets/T6_GGJ/T6_Scripts/T6_AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T6_AudioManager : MonoBehaviour
+{
+    public static T6_AudioManager am;
+
+    public AudioClip hit;
+
+    private void Awake()
+    {
+        if (am == null)
+            am = this;
+        else if (am != this)
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        AudioSource.PlayClipAtPoint(hit, Camera.main.transform.position, 1);
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+}
